9ebb6ace443534d818840333b071315be5ed8658
Amuktha Goteti <127406276+AmukthaGoteti@users.noreply.github.com>
2025-08-13 14:26:01 +0530

    Add files via upload

---
+++ Python_Introduction.py
@@ -0,0 +1,10 @@
+# This is Comment. It will not be executed.
+print("Hello, World!")
+"""
+This is a multiline comment.
+It can be used to describe larger sections of code.
+"""
+# Indentation in Python
+print("I have no indentation")
+
+    #print("I have tab indentaion") # Uncommenting the line below will cause an IndentationError
